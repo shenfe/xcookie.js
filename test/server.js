9fa2f6800d7f77eb47bb181c16d80ea0655b377d
hengwu <youchuangqiba@163.com>
2017-08-21 23:08:18 +0800

    add: whitelist, blacklist.

--- test/server.js
+++ test/server.js
@@ -5,6 +5,10 @@ const cookieParser = require('cookie-parser');
 const open = require('open');
 const ip = require('ip');
 
+fs.writeFileSync(path.resolve(process.cwd(), './test/xcookie.html'),
+    fs.readFileSync(path.resolve(process.cwd(), './src/xcookie.html'), 'utf8')
+        .replace('/* Define a whitelist of cookie names here. */', '"127_0_0_1"'));
+
 function launchServer(port, path) {
     let app = express();
     app.use(cookieParser());
@@ -22,7 +26,7 @@ function launchServer(port, path) {
 }
 
 launchServer(4010, 'test');
-launchServer(4011, 'src');
+launchServer(4011, 'test');
 launchServer(4012, 'dist');
 
 let ipAddr = ip.address();
